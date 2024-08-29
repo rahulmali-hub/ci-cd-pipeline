179be1748d6c75b380a1fae02d758dd2ffe03bbe
rahul mali <65352582+rahulmali-hub@users.noreply.github.com>
2024-08-29 15:52:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port=4400;
 app.get('/getuser',(req,res)=>{
 return res.send("hello users")
 })
-
+app.get('/message',(req,res)=>{
+    return res.send("message")
+})
 app.listen(port,()=>{
     console.log(`server is runing on ${port}...`)
 })
